f0adb178429fa2e5cfced9287c31671e069a7512
Jonathan Zempel <jzempel@gmail.com>
2020-07-27 10:27:31 -0700

    Fix scoping

--- src/scripts/off.ts
+++ src/scripts/off.ts
@@ -5,36 +5,37 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-const removeHighlight = (component: HTMLElement) => {
-  component.style.boxShadow = '';
-  component.style.outline = '';
-};
-
-const restoreTitle = (component: Element) => {
-  const ATTRIBUTE_GARDEN_TITLE = 'data-garden-title';
-  const title = component.getAttribute(ATTRIBUTE_GARDEN_TITLE);
-
-  if (title) {
-    component.setAttribute('title', title);
-    component.removeAttribute(ATTRIBUTE_GARDEN_TITLE);
-  } else {
-    component.removeAttribute('title');
-  }
-};
-
-const uninspect = (doc: Document) => {
+(() => {
   const ATTRIBUTE_GARDEN_ID = 'data-garden-id';
   const ATTRIBUTE_GARDEN_CONTAINER_ID = 'data-garden-container-id';
-  const selector = `[${ATTRIBUTE_GARDEN_ID}], [${ATTRIBUTE_GARDEN_CONTAINER_ID}]`;
-  const components = <HTMLElement[]>(<unknown>doc.querySelectorAll(selector));
+  const ATTRIBUTE_GARDEN_TITLE = 'data-garden-title';
 
-  components.forEach(component => {
-    removeHighlight(component);
-    restoreTitle(component);
-  });
-};
+  const removeHighlight = (component: HTMLElement) => {
+    component.style.boxShadow = '';
+    component.style.outline = '';
+  };
 
-(() => {
+  const restoreTitle = (component: Element) => {
+    const title = component.getAttribute(ATTRIBUTE_GARDEN_TITLE);
+
+    if (title) {
+      component.setAttribute('title', title);
+      component.removeAttribute(ATTRIBUTE_GARDEN_TITLE);
+    } else {
+      component.removeAttribute('title');
+    }
+  };
+
+  const uninspect = (doc: Document) => {
+    const selector = `[${ATTRIBUTE_GARDEN_ID}], [${ATTRIBUTE_GARDEN_CONTAINER_ID}]`;
+    const components = <HTMLElement[]>(<unknown>doc.querySelectorAll(selector));
+
+    components.forEach(component => {
+      removeHighlight(component);
+      restoreTitle(component);
+    });
+  };
+
   uninspect(document);
 
   const iframes = document.getElementsByTagName('iframe');
